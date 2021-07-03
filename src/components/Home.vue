6a0cfce948eb7471313ba3909cf5ef9be9c5c65b
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-07-04 01:30:33 +0900

    start vuetify[add]

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="home">
+        <h1>Home</h1>
+        <p class="blue lighten-5-red--text">Lorem ipsum dolor sit amet.</p>
+        <p class="blue white--text">Lorem ipsum dolor sit amet.</p>        
+        <p class="blue darken-3">Lorem ipsum dolor sit amet.</p>
+    </div>
+</template>
+<style>
+h1{margin:10px;}
+p{padding:10px ; margin: 5px 10px;}
+</style>
