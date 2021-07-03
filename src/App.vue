c9d8fce0d63add8219c30b64c881e545b432331a
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-07-03 22:12:06 +0900

    add navigater[add]

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
-    <div id="app">
-        <div>
-            <div class="container">
-                <nav class="cyan">
-                    <div class="nav-wrapper">
-                        <a href="#" class="brand-logo">Groad</a>
-
-                        <ul class="right">
-                            <li v-for="link in navlinks" :key="link.text">
-                            <a :href="link.href">{{link.text}}</a></li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </div>
-    </div>
+  <div id="app">
+    <h1>vfor 테스트</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>도서번호</th><th>도서제목</th><th>출판사</th><th>작가</th><th>재고</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in books" v-bind:key="item.id">
+          <td>{{item.id}}</td>
+          <td>{{item.title}}</td>
+          <td>{{item.pub}}</td>
+          <td>{{item.aut}}</td>
+          <td>{{item.stock}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
-<script>
-    new Vue({
-        el:'#app',
-        data:{
-            navlinks:[
-                {text:'마음의 순례길',href:'#pilgrimage'},
-                {text:'여행 도우미',href:'#triphelp'},
-                {text:'순례길 이야기',href:'#story'},
-                {text:'여행정보',href:'#tripinfo'},
-                {text:'커뮤니티',href:'#cummunity'}
-            ]
-        }
-    })
-</script>
+
+
+<style>
+  table{
+    width: 100%;
+    border-collapse: collapse;
+  }
+  tr:nth-of-type(odd){
+    background: #eee;
+  }
+  th{
+    background: #333;
+    color:white;
+    font-weight: bold;
+  }
+  td,th{
+    padding: 6px;
+    border: 1px solid #ccc;
+    text-align: left;
+  }
+</style>
---
+++ src/default/App1.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <div>
+            <div class="container">
+                <nav class="cyan">
+                    <div class="nav-wrapper">
+                        <a href="#" class="brand-logo">Groad</a>
+
+                        <ul class="right">
+                            <li v-for="link in navlinks" :key="link.text">
+                            <a :href="link.href">{{link.text}}</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    new Vue({
+        el:'#app',
+        data:{
+            navlinks:[
+                {text:'마음의 순례길',href:'#pilgrimage'},
+                {text:'여행 도우미',href:'#triphelp'},
+                {text:'순례길 이야기',href:'#story'},
+                {text:'여행정보',href:'#tripinfo'},
+                {text:'커뮤니티',href:'#cummunity'}
+            ]
+        }
+    })
+</script>
