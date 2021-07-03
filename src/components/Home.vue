4e2684868a3a7276db9639b8bb9fe659b4cf152f
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-07-04 04:01:44 +0900

    visble button test[add]

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,9 +4,9 @@
         <p class="blue lighten-5-red--text">Lorem ipsum dolor sit amet.</p>
         <p class="blue white--text">Lorem ipsum dolor sit amet.</p>        
         <p class="blue darken-3">Lorem ipsum dolor sit amet.</p>
-<v-btn color="blue" class="hide-xs-only">모바일 화면일때 보임</v-btn>        
-<v-btn color="blue" class="hide-sm-only">태블릿 화면일때 보임</v-btn>
-<v-btn color="blue" class="hide-md-only">데탑 모니터일때 보임</v-btn>
+<v-btn color="blue" class="hidden-xs-only">모바일 화면일때 보임</v-btn>        
+<v-btn color="blue" class="hidden-sm-only">태블릿 화면일때 보임</v-btn>
+<v-btn color="blue" class="hidden-md-only">데탑 모니터일때 보임</v-btn>
     </div>
 </template>
 <style>
