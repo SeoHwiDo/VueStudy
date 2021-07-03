207b9442d1c06edcbaeb55e1881c85d11f893cc3
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-07-04 03:42:48 +0900

    add vuetify button[add]

--- src/components/Homebutton.vue
+++ src/components/Homebutton.vue
@@ -5,9 +5,10 @@
         <v-btn color="teal" text>기본버튼</v-btn>
         <v-btn color="teal" dark>
             <v-icon>mdi-bluetooth</v-icon>
+            <span>bluetooth</span>
         </v-btn>
         <v-btn fab color="teal" dark>
-            
+            <v-icon>mdi-bluetooth</v-icon>
         </v-btn>
     </div>
 </template>
