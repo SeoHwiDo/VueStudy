edf250dab20245487f5f3b41d1815034a03379cb
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-07-03 17:06:38 +0900

    reset

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <template>
   <div id="app">
-    <p>{{messege}}</p>
     <img alt="Vue logo" src="./assets/logo.png">
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
- <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-    <script type="text/javascript">
-    var vm=new Vue({
-      el:'#app',
-      data:{
-        messege:'뷰 인스턴스 테스트'
-      }
-    })
-    </script>
+
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
