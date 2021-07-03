b8bb048c7c6acd1ef67f1cc42da1ff3624be1a0a
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-07-03 21:10:55 +0900

    add navigater[add]

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@ new Vue({
     data:{
         navlinks:[
             {text:'마음의 순례길',href:'#pilgrimage'},
-            {text:'여행 도우미',href:'triphelp'},
-            {text:'순례길 이야기',href:'story'},
-            {text:'여행정보',href:'tripinfo'},
-            {text:'커뮤니티',href:'cummunity'}
+            {text:'여행 도우미',href:'#triphelp'},
+            {text:'순례길 이야기',href:'#story'},
+            {text:'여행정보',href:'#tripinfo'},
+            {text:'커뮤니티',href:'#cummunity'}
         ]
     }
 })
