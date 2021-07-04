c4f68b907260f87fae02a7c8c590b950f8b78f18
SeoHwiDo <baeksa01@hallym.ac.kr>
2021-07-04 11:24:26 +0900

    add toolbar[add]

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,9 +1,12 @@
 <template>
-    <v-toolbar-items class=hidden-sm-and-down>
+<div class="toolbar">
+   <v-toolbar-items class=hidden-sm-and-down>
         <v-btn text>마음의 순례길</v-btn>
         <v-btn text>여행 도우미</v-btn>
         <v-btn text>순례길 이야기</v-btn>
         <v-btn text>여행 정보</v-btn>
         <v-btn text>커뮤니티</v-btn>
     </v-toolbar-items>
+</div>
+ 
 </template>
